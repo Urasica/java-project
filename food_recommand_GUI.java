94111690c4f78997121eae8dfa3e16a72a6faff5
EunJung Kim <dlqnfdl@gmail.com>
2023-11-14 00:44:48 +0900

    종류화면 추가

--- food_recommand_GUI.java
+++ food_recommand_GUI.java
@@ -2,16 +2,20 @@ import javax.swing.*;
 import java.awt.*;
 
 public class food_recommand_GUI extends JFrame {
+    static CardLayout cardLayout;
+    static Container c;
     food_recommand_GUI() {
         super("음식메뉴추천");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Container c = getContentPane();
+        c = getContentPane();
 
-        CardLayout cardLayout = new CardLayout();
+        cardLayout = new CardLayout();
         c.setLayout(cardLayout);
         JPanel start = new startPanel();
+        JPanel selectKind = new selectKindPanel();
 
         c.add(start);
+        c.add(selectKind);
 
         setSize(400, 600);
         setVisible(true);
