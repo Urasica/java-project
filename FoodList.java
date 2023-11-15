4c0fb7d820b0b43636ab25c306020a689b78e209
Minwook Eom <minwook2000@naver.com>
2023-11-16 00:55:07 +0900

    음식이름을 파일입출력으로 txt파일에 작성

---
+++ FoodList.java
@@ -0,0 +1,16 @@
+public class FoodList extends FoodName{
+	
+	public Food createFoodItem() {
+		Food FoodItem = new Food();
+		
+		
+		
+		return FoodItem;
+	}
+	
+	
+	public FoodList(){
+		
+
+	}
+}
