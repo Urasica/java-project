905401dea0050966c66ae1e84729fb9b05ec5f3b
Minwook Eom <minwook2000@naver.com>
2023-12-09 21:20:58 +0900

    배경음악 삽입 및 조절, 설정 패널 변경

--- src/food_recommand_GUI.java
+++ src/food_recommand_GUI.java
@@ -23,7 +23,7 @@ public class food_recommand_GUI extends JFrame {
         JPanel setting = new settingPanel();
         JPanel selectKind = new selectKindPanel();
         JPanel selectFavorite = new selectFavoritePanel();
-
+        backgroundMusic.playSound("backgroundmusic.wav",volume); //배경음악 삽입
         // 생성한 패널들 부착
         c.add(start);
         c.add(setting);
